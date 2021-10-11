7482973ca58d26b6bd8e89d6eed0588f9af5fbe8
Serhii Kovtunenko <serhii.kovtunenko@gmail.com>
2021-10-11 13:48:41 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Serhii Kovtunenko on 11.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
